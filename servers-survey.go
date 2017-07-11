9ddde69de795d826c7048b1740d150b87d95d5cc
Rich Scott <richscott@sent.com>
2017-07-10 22:05:53 -0600

    Use struct to return results; render final survey map.

--- servers-survey.go
+++ servers-survey.go
@@ -1,62 +1,93 @@
 package main
 
 import (
-  "encoding/csv"
-  "fmt"
-  "io"
-  "log"
-  "net/http"
-  "os"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
-func surveySite(siteDomain string, results chan<- string) {
-  serverSoftware := "<unknown>"
+type surveyResult struct {
+	site           string
+	serverSoftware string
+}
 
-  resp, err := http.Get(fmt.Sprintf("http://%s", siteDomain))
-  if err != nil {
-    log.Print(err)
-    results <- serverSoftware
-    return
-  }
-  defer resp.Body.Close()
+func surveyWorker(id int, siteJobs <-chan string, results chan<- surveyResult) {
+	serverSW := "<unknown>"
 
-  if len(resp.Header.Get("Server")) > 0 {
-    serverSoftware = resp.Header.Get("Server")
-  }
-  fmt.Printf("%s -> %s\n", siteDomain, serverSoftware)
-  results <- serverSoftware
+	for hostname := range siteJobs {
+		fmt.Printf("Worker %3d = surveying %s\n", id, hostname)
+    // TODO set an aggressive short timeout on this
+		resp, err := http.Get(fmt.Sprintf("http://%s", hostname))
+		if err != nil {
+			log.Print(err)
+			results <- surveyResult{site: hostname, serverSoftware: serverSW}
+			return
+		}
+		defer resp.Body.Close()
+
+		if len(resp.Header.Get("Server")) > 0 {
+			serverSW = resp.Header.Get("Server")
+		}
+		results <- surveyResult{site: hostname, serverSoftware: serverSW}
+	}
 }
 
 func main() {
-  siteListFile, err := os.Open("fortune500-2014.csv")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer siteListFile.Close()
+	siteListFile, err := os.Open("fortune500-2014.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer siteListFile.Close()
 
-	sites := make(map[string]string)
+	sites := make([]string, 0)
+	r := csv.NewReader(siteListFile)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		siteDomain := record[2]
+		sites = append(sites, siteDomain)
+	}
 
-	serverChan := make(chan string)
+	siteServer := make(map[string]string)
+	jobs := make(chan string, len(sites))
+	results := make(chan surveyResult, len(sites))
 
-  r := csv.NewReader(siteListFile)
+	// Start up the pool of workers (just 5 right now).
+	// They will block until they have something to do,
+	// which arrives to them via the jobs channel.
+	for w := 1; w <= 50; w++ {
+		go surveyWorker(w, jobs, results)
+	}
 
-  for {
-    record, err := r.Read()
-    if err == io.EOF {
-      break
+	for _, site := range sites {
+		jobs <- site
+	}
+
+	for n := 1; n <= (len(sites) - 10); n++ {
+		res := <-results
+		siteServer[res.site] = res.serverSoftware
+	}
+
+	serverCount := make(map[string]int)
+
+	for _, software := range siteServer {
+    if count, exists := serverCount[software]; exists {
+      serverCount[software] = count + 1
+    } else {
+      serverCount[software] = 1
     }
-    if err != nil {
-      log.Fatal(err)
-    }
-    siteDomain := record[2]
+	}
 
-    go surveySite(siteDomain, serverChan)
-    serverSoftware := <-serverChan
-
-    sites[siteDomain] = serverSoftware
+  for software, count := range serverCount {
+    fmt.Printf("%3d  %-50s\n", count, software)
   }
 
-  //for site, software := range sites {
-  //  fmt.Printf("%s -> %s\n", site, software)
-  //}
 }
